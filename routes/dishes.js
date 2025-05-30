fea3837bcb07973480466742e7b75363d0f419ab
jGarzon13 <juliangarzontoro@gmail.com>
2025-05-30 13:53:20 -0500

    imagenes de sitios

--- routes/dishes.js
+++ routes/dishes.js
@@ -61,8 +61,8 @@ router.get('/seed', async (req, res) => {
       { name: 'Arroz con Coco', siteId: ciudad_amurallada._id, price: 12000, description: 'Arroz dulce caribeño', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsmKJe3S7gWLxU2U5Xq9IkVOA4JlTgmnG2LQ&s' },
       { name: 'Empanadas', siteId: museo_oro._id, price: 5000, description: 'Empanadas de carne', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6MTS9nwQeRBI7fvNeWbVMsFAigBXI9kHBAw&s' },
       // Italia: 10 platos tradicionales
-      { name: 'Pizza Margherita', siteId: coliseo._id, price: 12000, description: 'Pizza clásica italiana', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY7RbPLpeQGEKr-JMLB6L9kRdCTMtUbFJfJw&s },
-      { name: 'Pasta Carbonara', siteId: coliseo._id, price: 15000, description: 'Pasta con huevo y panceta', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDNgnddUmlzqZgpQc5PHUYCUc0BMRS7S2eXg&s },
+      { name: 'Pizza Margherita', siteId: coliseo._id, price: 12000, description: 'Pizza clásica italiana', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY7RbPLpeQGEKr-JMLB6L9kRdCTMtUbFJfJw&s' },
+      { name: 'Pasta Carbonara', siteId: coliseo._id, price: 15000, description: 'Pasta con huevo y panceta', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDNgnddUmlzqZgpQc5PHUYCUc0BMRS7S2eXg&s' },
       { name: 'Lasagna', siteId: catedral_milan._id, price: 18000, description: 'Pasta en capas con carne y queso', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ73xyJnJvRAGkOybHGbacQQa8a-uAArfOgMQ&s' },
       { name: 'Risotto', siteId: catedral_milan._id, price: 16000, description: 'Arroz cremoso con azafrán', imageUrl: 'https://images.cookforyourlife.org/wp-content/uploads/2018/08/shutterstock_53574826.jpg' },
       { name: 'Tiramisú', siteId: coliseo._id, price: 8000, description: 'Postre con café y mascarpone', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKs2DA-_PIKEEOtohBOBKRF7QLHqN7um7ckQ&s' },
@@ -70,7 +70,7 @@ router.get('/seed', async (req, res) => {
       { name: 'Gnocchi', siteId: coliseo._id, price: 14000, description: 'Dumplings de papa', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQmcZzeqYkMrGiz1eaxjKG_KeJQof_E968SQ&s' },
       { name: 'Pesto alla Genovese', siteId: catedral_milan._id, price: 10000, description: 'Salsa de albahaca y ajo', imageUrl: 'https://assets.tmecosys.com/image/upload/t_web_rdp_recipe_584x480/img/recipe/ras/Assets/A84C97BB-F426-49D2-89C0-CE4D51DA114C/Derivates/A5E989EE-8E12-41B1-BB46-7E0C8C7495DF.jpg' },
       { name: 'Arancini', siteId: coliseo._id, price: 6000, description: 'Bolitas de arroz fritas', imageUrl: 'https://assets.tmecosys.com/image/upload/t_web_rdp_recipe_584x480/img/recipe/ras/Assets/0635BB65-320C-47B7-B52A-555E986646D9/Derivates/28fad4c4-c3b1-4018-b4b5-743b3ce4ad99.jpg' },
-      { name: 'Gelato', siteId: coliseo._id, price: 5000, description: 'Helado italiano', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmBwiv4nk21wE4aCYrsiFiVYHCBeoMful5UQ&s },
+      { name: 'Gelato', siteId: coliseo._id, price: 5000, description: 'Helado italiano', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmBwiv4nk21wE4aCYrsiFiVYHCBeoMful5UQ&s'},
       // Alemania: 10 platos tradicionales
       { name: 'Schnitzel', siteId: muro_berlin._id, price: 15000, description: 'Milanesa de cerdo', imageUrl: 'https://thestayathomechef.com/wp-content/uploads/2024/04/Authentic-German-Schnitzel_Square-2.jpg' },
       { name: 'Sauerkraut', siteId: muro_berlin._id, price: 5000, description: 'Repollo fermentado', imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoYRsHzgfgBp0o--EyMqy8umG8yp-XIEeq3Q&s' },
